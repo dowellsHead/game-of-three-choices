cc81a9aeef081da11e0a6b3d2e1df35085dd4368
dowellsHead <xkopylova@gmail.com>
2026-03-10 01:22:49 +0300

    add game logic with multiple rounds and scoring

--- script.js
+++ script.js
@@ -33,7 +33,9 @@ function getHumanChoice() {
 
 }
 
-function playRound(humanChoice, computerChoice){
+function playRound(){
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice(humanChoice);
     let result = 'tie';
     if (humanChoice=='rock'){
         switch (computerChoice){
@@ -65,22 +67,46 @@ function playRound(humanChoice, computerChoice){
     }
     if (result=='computer'){
         console.log('the tin can won! ', computerChoice, ' beats ', humanChoice, '!');
-        computerScore++;
     } else if (result=='human'){
         console.log('the meat sack won! ', humanChoice, ' beats ', computerChoice, '!');
-        humanScore++;
     } else if (result=='tie'){
         console.log('this is a tie! would you like to upgrade it to a bow?');
     }
+    return result;
+
+}
+
+function playGame(num){
+
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    while (num>0) {
+        let scoring = playRound(humanScore, computerScore);
+        switch (scoring) {
+            case 'human':
+                humanScore++;
+                break;
+            case 'computer':
+                computerScore++;
+                break;
+            case 'tie':
+                break;
+        }
+        num--;
+    }
+
+    console.log('the score is thus\nhuman: ', humanScore, '; computer: ', computerScore);
+    if (humanScore>computerScore){
+        console.log('ultimate human victory over the machine! yay');
+    } else if (humanScore<computerScore){
+        console.log('they won chess, and now rock-paper-scissors??? ubelivable!!');
+    } else
+        console.log('ye, boring stuff, it\'s a tie, guys. refresh for another round');
 
 }
 
 
-let humanScore = 0;
-let computerScore = 0;
+let numberRound = prompt('give me a number of rounds, buddy');
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice(humanSelection);
-
-playRound(humanSelection, computerSelection, humanScore, computerScore);
-console.log('the score is thus\nhuman: ', humanScore, '; computer: ', computerScore);
+playGame(numberRound);
